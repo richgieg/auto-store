1ff2bc5e42cb26e15032a15b00508ca556edc8a2
Richard Gieg <richgieg@users.noreply.github.com>
2015-10-22 00:51:41 -0700

    Remove year field from automobiles

--- app/models.py
+++ app/models.py
@@ -16,7 +16,6 @@ class Automobile(db.Model):
     __tablename__ = 'automobiles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
-    year = db.Column(db.Integer, nullable=False)
     manufacturer_id = db.Column(db.Integer, db.ForeignKey('manufacturers.id'),
                                 nullable=False)
 
